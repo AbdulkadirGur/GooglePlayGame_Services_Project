03461985ec9e7455c24f6950a6e567d1cf95d094
Abdulkadir Gür <58683068+AbdulkadirGur@users.noreply.github.com>
2023-01-20 03:42:42 +0300

    GPGS

---
+++ GooglePlayGame_Services/Assets/Scripts/Achievement.cs
@@ -0,0 +1,132 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SocialPlatforms;
+using GooglePlayGames;
+
+public class Achievement : MonoBehaviour
+{
+    public Text logtext;
+    
+    public void ShowAchievementsUI()
+    {
+        Social.ShowAchievementsUI();
+    }
+
+    public void DoGrantAchievement(string _achievement)
+    {
+        Social.ReportProgress(_achievement,
+            100.00f,
+            (bool success) =>
+            {
+                if (success) // if success is true 
+                {
+                    logtext.text = _achievement + " : " + success.ToString();
+                    //perform new actions here on success
+                }
+                else
+                {
+                    logtext.text = _achievement + " : " + success.ToString();
+                    //perform new actions here on failed
+                }
+            });
+
+    }
+
+    public void DoIncrementalAchievement(string _achievement)
+    {
+        PlayGamesPlatform platform = (PlayGamesPlatform)Social.Active;
+
+        platform.IncrementAchievement(_achievement,
+            1,
+            (bool success) =>
+            {
+                if (success) // if success is true 
+                {
+                    logtext.text = _achievement + " : " + success.ToString();
+                    //perform new actions here on success
+                }
+                else
+                {
+                    logtext.text = _achievement + " : " + success.ToString();
+                    //perform new actions here on failed
+                }
+            });
+    }
+
+
+    public void DoRevealAchievement(string _achievement)
+    {
+        Social.ReportProgress(_achievement,
+            1.00f,
+            (bool success) =>
+            {
+                if (success) // if success is true 
+                {
+                    logtext.text = _achievement + " : " + success.ToString();
+                    //perform new actions here on success
+                }
+                else
+                {
+                    logtext.text = _achievement + " : " + success.ToString();
+                    //perform new actions here on failed
+                }
+            });
+
+    }
+
+
+    public void ListAchievements()
+    {
+        Social.LoadAchievements(achievements =>
+        {
+            logtext.text = "Loaded Achievements" + achievements.Length;
+            foreach (IAchievement ach in achievements)
+            {
+                logtext.text += "/n" + ach.id + " " + ach.completed;
+            }
+        });
+    }
+
+    public void ListDescriptions()
+    {
+        Social.LoadAchievementDescriptions(achievements =>
+        {
+            logtext.text = "Loaded Achievements" + achievements.Length;
+            foreach (IAchievementDescription ach in achievements)
+            {
+                logtext.text += "/n" + ach.id + " " + ach.title;
+            }
+        });
+    }
+
+    public void GrantAchievementBtn()
+    {
+        DoGrantAchievement(Code_Test.GPGSids.achievement_unlock_achievement);
+    }
+
+    public void GrantIncrementalBtn()
+    {
+        DoGrantAchievement(Code_Test.GPGSids.achievement_increment_achievement);
+    }
+    public void RevealAchievementBtn()
+    {
+        DoGrantAchievement(Code_Test.GPGSids.achievement_hidden_unlock_achievement);
+    }
+
+    public void RevealIncrementalBtn()
+    {
+        DoGrantAchievement(Code_Test.GPGSids.achievement_hidden_incremental_achievement);
+    }
+
+    public void GrantHiddenAchievementBtn()
+    {
+        DoGrantAchievement(Code_Test.GPGSids.achievement_hidden_unlock_achievement);
+    }
+
+    public void HiddenlIncrementalAchievementBtn()
+    {
+        DoGrantAchievement(Code_Test.GPGSids.achievement_hidden_incremental_achievement);
+    }
+}
---
+++ GooglePlayGame_Services/Assets/Scripts/GPGSManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.UI;
+using GooglePlayGames;
+using GooglePlayGames.Android;
+
+
+using UnityEngine.SocialPlatforms;
+
+public class GPGSManager : MonoBehaviour
+{
+    private PlayGamesClientConfiguration clientConfigurations;
+    public Text statusTxt;
+    public Text descriptionTxt;
+    public GameObject HomeBtnGo;
+
+    void Start()
+    {
+        ConfigureGPGS();
+    }
+
+    internal void ConfigureGPGS()
+    {
+        clientConfigurations = new PlayGamesClientConfiguration.Builder().Build();
+        SignIntoGPGS(SignInInteractivity.CanPromptOnce, clientConfigurations);
+    }
+
+
+    internal void SignIntoGPGS(SignInInteractivity interactivity, PlayGamesClientConfiguration configuration)
+    {
+        configuration = clientConfigurations;
+        PlayGamesPlatform.InitializeInstance(configuration);
+        PlayGamesPlatform.Activate();
+
+        PlayGamesPlatform.Instance.Authenticate(interactivity, (code) =>
+       {
+           statusTxt.text = "Authenticating...";
+           if (code == SignInStatus.Success)
+           {
+               statusTxt.text = "Successfully Authenticated";
+               descriptionTxt.text = "Hello " + Social.localUser.userName + "You have an ID of " + Social.localUser.id;
+               HomeBtnGo.SetActive(true);
+           }
+           else
+           {
+               statusTxt.text = "Failed to Authenticate";
+               descriptionTxt.text = "Failed to Authenticate, reason for failure is " + code;
+
+           }
+       });
+
+    }
+
+    public void BasicSignInBtn()
+    {
+        SignIntoGPGS(SignInInteractivity.CanPromptAlways, clientConfigurations);
+    }
+    public void SignOutBtn()
+    {
+        PlayGamesPlatform.Instance.SignOut();
+        statusTxt.text = "Signed Out ";
+        descriptionTxt.text = "";
+        HomeBtnGo.SetActive(false);
+    }
+
+}
